69534e088581fd1f84c0059b35efaa7a1e65db83
mtunique <taomeng@hustunique.com>
2014-07-19 13:36:54 +0800

    true

--- main.cpp
+++ main.cpp
@@ -13,32 +13,13 @@ void exit_with_help()
 	printf(
            "Usage: svm-train [options] training_set_file [model_file]\n"
            "options:\n"
-           "-s svm_type : set type of SVM (default 0)\n"
-           "	0 -- C-SVC		(multi-class classification)\n"
-           "	1 -- nu-SVC		(multi-class classification)\n"
-           "	2 -- one-class SVM\n"
-           "	3 -- epsilon-SVR	(regression)\n"
-           "	4 -- nu-SVR		(regression)\n"
            "-t kernel_type : set type of kernel function (default 2)\n"
            "	0 -- linear: u'*v\n"
            "	1 -- polynomial: (gamma*u'*v + coef0)^degree\n"
            "	2 -- radial basis function: exp(-gamma*|u-v|^2)\n"
            "	3 -- sigmoid: tanh(gamma*u'*v + coef0)\n"
            "	4 -- precomputed kernel (kernel values in training_set_file)\n"
-           "-d degree : set degree in kernel function (default 3)\n"
-           "-g gamma : set gamma in kernel function (default 1/num_features)\n"
-           "-r coef0 : set coef0 in kernel function (default 0)\n"
-           "-c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR (default 1)\n"
-           "-n nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR (default 0.5)\n"
-           "-p epsilon : set the epsilon in loss function of epsilon-SVR (default 0.1)\n"
-           "-m cachesize : set cache memory size in MB (default 100)\n"
-           "-e epsilon : set tolerance of termination criterion (default 0.001)\n"
-           "-h shrinking : whether to use the shrinking heuristics, 0 or 1 (default 1)\n"
-           "-b probability_estimates : whether to train a SVC or SVR model for probability estimates, 0 or 1 (default 0)\n"
-           "-wi weight : set the parameter C of class i to weight*C, for C-SVC (default 1)\n"
-           "-v n: n-fold cross validation mode\n"
-           "-q : quiet mode (no outputs)\n"
-           );
+                      );
 	exit(1);
 }
 
@@ -147,61 +128,9 @@ void parse_command_line(int argc, char **argv, char *input_file_name, char *mode
 			exit_with_help();
 		switch(argv[i-1][1])
 		{
-			case 's':
-				param.svm_type = atoi(argv[i]);
-				break;
+
 			case 't':
 				param.kernel_type = atoi(argv[i]);
-				break;
-			case 'd':
-				param.degree = atoi(argv[i]);
-				break;
-			case 'g':
-				param.gamma = atof(argv[i]);
-				break;
-			case 'r':
-				param.coef0 = atof(argv[i]);
-				break;
-			case 'n':
-				param.nu = atof(argv[i]);
-				break;
-			case 'm':
-				param.cache_size = atof(argv[i]);
-				break;
-			case 'c':
-				param.C = atof(argv[i]);
-				break;
-			case 'e':
-				param.eps = atof(argv[i]);
-				break;
-			case 'p':
-				param.p = atof(argv[i]);
-				break;
-			case 'h':
-				param.shrinking = atoi(argv[i]);
-				break;
-			case 'b':
-				param.probability = atoi(argv[i]);
-				break;
-			case 'q':
-				print_func = &print_null;
-				i--;
-				break;
-			case 'v':
-				cross_validation = 1;
-				nr_fold = atoi(argv[i]);
-				if(nr_fold < 2)
-				{
-					fprintf(stderr,"n-fold cross validation: n must >= 2\n");
-					exit_with_help();
-				}
-				break;
-			case 'w':
-				++param.nr_weight;
-				param.weight_label = (int *)realloc(param.weight_label,sizeof(int)*param.nr_weight);
-				param.weight = (double *)realloc(param.weight,sizeof(double)*param.nr_weight);
-				param.weight_label[param.nr_weight-1] = atoi(&argv[i-1][2]);
-				param.weight[param.nr_weight-1] = atof(argv[i]);
 				break;
 			default:
 				fprintf(stderr,"Unknown option: -%c\n", argv[i-1][1]);
